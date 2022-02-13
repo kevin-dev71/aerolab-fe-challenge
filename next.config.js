5788072f42fa2f540cd6ad42f13a9ab905314a6f
Kevin Dev <kevin.developer71@gmail.com>
2022-02-12 20:20:42 -0600

    styled components config added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    // ssr and displayName are configured by default
+    styledComponents: true,
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
