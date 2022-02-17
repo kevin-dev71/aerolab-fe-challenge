5e717d04d0416778f5ccdeb3c35124dac256060b
Kevin Dev <kevin.developer71@gmail.com>
2022-02-16 21:23:52 -0600

    fix description in meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,31 +9,6 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Aerolab | Frontend challenge</title>
-        {/* <!-- Open Graph / Facebook --> */}
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://aerolab-fe-challenge.vercel.app/" />
-        <meta property="og:title" content="Frutti ¡STOP!" />
-        <meta
-          property="og:description"
-          content="El mejor juego de Tutti Frutti, ¡STOP! o Basta! llevado a lo digital, para disfrutar con tus amigos y pasar un buen rato."
-        />
-        <meta
-          property="og:image"
-          content="https://aerolab-fe-challenge.vercel.app/images/metadata-SEO.png?raw=true"
-        />
-
-        {/* <!-- Twitter --> */}
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://aerolab-fe-challenge.vercel.app/" />
-        <meta property="twitter:title" content="Frutti ¡STOP!" />
-        <meta
-          property="twitter:description"
-          content="El mejor juego de Tutti Frutti, ¡STOP! o Basta! llevado a lo digital, para disfrutar con tus amigos y pasar un buen rato."
-        />
-        <meta
-          property="twitter:image"
-          content="https://aerolab-fe-challenge.vercel.app/images/metadata-SEO.png?raw=true"
-        />
       </Head>
       <Layout>
         <Component {...pageProps} />
